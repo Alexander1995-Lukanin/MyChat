7b0a392e1bd4a1beaca2504a1921e29b553a7c63
Alexander <kungfpanda@mail.ru>
2022-02-19 23:37:07 +0300

    BD Auth

--- chat-server/src/main/java/DatabaseChat/Database.java
+++ chat-server/src/main/java/DatabaseChat/Database.java
@@ -1,103 +1,93 @@
 package DatabaseChat;
 
+import error.ChangeNickExeption;
 import error.WrongCredentialsException;
 
 import java.sql.*;
 
 public class Database {
-    private static Connection connection;
-    private static Statement statement;
-    private static PreparedStatement ps;
-    private static String UPDATE_Statement = "UPDATE accounts (nick) values (?) ;";
-    private static final String EXAMPLE_CALL = "{call do_something_prc(?,?,?,?)}";
-    private static final String DB_CONNECTION_STRING = "jdbc:sqlite:db/accounts.db";
-    private static final String CREATE_REQUEST = "create table if not exists accounts " +
-            "(id integer primary key autoincrement, login text, password text, nick text, secret text);";
-    private static final String DROP_REQUEST = "drop table if exists accounts;";
-    private static final String SIMPLE_INSERT_REQUEST =
-            "insert into accounts (login, password, nick, secret) values ('log1','pass1','nick1','secret1'), " +
-                    "('log2', 'pass2','nick2','secret2'), ('log3', 'pass3','nick3', 'secret3');";
+    private static final String DRIVER = "org.sqlite.JDBC";
+    private static final String CONNECTION = "jdbc:sqlite:db/clients.db";
+    private static final String GET_USERNAME = "select username from clients where login = ? and password=?;";
+    private static final String CHANGE_USERNAME = "update clients set username = ? where login = ?;";
+    private static final String CREATE_DB = "create table if not exists clients (id integer primary key autoincrement)" +
+            " login text unique not null, password text not null, username text unique not null);";
+    private static final String INIT_DB = "insert into clients (login, password, username) values ('log1','pass1','user1'), " +
+            "('log2','pass2','user2'), ('log2','pass2','user2');";
+    private static Database instance;
+    private Connection connection;
+    PreparedStatement getClientStatement;
+    PreparedStatement changeNickStatement;
 
-    private static void massInsertBatchExample()  throws SQLException  {
-        var start = System.currentTimeMillis();
-        connection.setAutoCommit(false);
-        for (int i = 0; i < 5000; i++) {
-            ps.setInt(2, i);
-            ps.setString(1, "Student #" + i + 1);
-            ps.addBatch();
+
+    public Database() {
+        try {
+            connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
-        ps.executeBatch();
-        connection.setAutoCommit(true);
-        System.out.println(System.currentTimeMillis() - start);
+        createDb();
     }
 
-
-    public static String preparedStatementExample(String login, String nick2)  throws SQLException  {
-        ps = connection.prepareStatement(UPDATE_Statement);
-            ps.setString(1,login);
-        if(ps.equals(login)) {
-            ps.setString(3,nick2);
-        }
-        return nick2;
+    public static Database getInstance() {
+        if (instance != null) return instance;
+        instance = new Database();
+        return instance;
     }
 
+    public String changeUsername(String login, String newNick) throws SQLException {
+        try {
+            changeNickStatement.setString(1, newNick);
+            changeNickStatement.setString(2, login);
+            if (changeNickStatement.executeUpdate() > 0) return newNick;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        throw new ChangeNickExeption("Something went wrong");
+    }
 
-    public static String AuthRead (String login, String password)  throws SQLException  {
-        try (var resultSet = statement.executeQuery("select login, password, nick from accounts " +
-                "where password =? and login=? and nick=?" )) {
-            String log = resultSet.getString("login");
-            String pass  = resultSet.getString("password");
-            String nick  = resultSet.getString("nick");
-            if (log.equals(login) & pass.equals(password)) {
-                return nick;
+    public String getClientNameByLoginPass(String login, String pass) {
+        try {
+            getClientStatement.setString(1, login);
+            getClientStatement.setString(2, pass);
+            ResultSet rs = getClientStatement.executeQuery();
+            if (rs.next()) {
+                String result = rs.getString("username");
+                rs.close();
+                System.out.println(result);
+                return result;
             }
-            throw new WrongCredentialsException("Wrong username or password");
-        }
-    }
-
-//    public static void simpleUpdateExample()  throws SQLException  {
-//        var count = statement.executeUpdate("update students set name = 'Alex' where score > 90;");
-//        System.out.printf("Updated %d rows\n", count);
-//    }
-//
-//    public static void dropExample()  throws SQLException  {
-//        statement.execute(DROP_REQUEST);
-//    }
-//
-//    public static void simpleInsertExample()  throws SQLException  {
-//        var count = statement.executeUpdate(SIMPLE_INSERT_REQUEST);
-//        System.out.printf("Inserted %d rows\n", count);
-//    }
-
-    public static void createTable() throws SQLException {
-        statement.execute(CREATE_REQUEST);
-    }
-
-
-    public static void connect() throws SQLException   {
-        connection = DriverManager.getConnection(DB_CONNECTION_STRING);
-        statement = connection.createStatement();
-        //ps = connection.prepareStatement(insertStatement);
-    }
-
-    public static void disconnect() {
-        try {
-            if (statement != null) statement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        throw new WrongCredentialsException("User not found");
+    }
 
-        try {
-            if (ps != null) ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+    private void createDb() {
+        try (Statement st = connection.createStatement()) {
+            st.execute(CREATE_DB);
+            st.execute(INIT_DB);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+    }
 
+    private void connect() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+        connection = DriverManager.getConnection(CONNECTION);
+        System.out.println("Connected to db");
+        getClientStatement = connection.prepareStatement(GET_USERNAME);
+        changeNickStatement = connection.prepareStatement(CHANGE_USERNAME);
+    }
+
+    public void closeConnection() {
         try {
+            if (getClientStatement != null) getClientStatement.close();
+            if (changeNickStatement != null) changeNickStatement.close();
             if (connection != null) connection.close();
+            System.out.println("Disconnected from db");
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
---
+++ chat-server/src/main/java/error/ChangeNickExeption.java
@@ -0,0 +1,5 @@
+package error;
+
+public class ChangeNickExeption extends RuntimeException {
+    public  ChangeNickExeption(String message){ super((message));}
+}
